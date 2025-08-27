5ff65bed0f6540cc15747638f1b023a597d7acc2
Trevor Shires <goris295@gmail.com>
2025-08-27 16:28:49 -0400

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { ChatRoom } from "@/components/chat-room"
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-background">
+      <ChatRoom />
+    </main>
+  )
+}
